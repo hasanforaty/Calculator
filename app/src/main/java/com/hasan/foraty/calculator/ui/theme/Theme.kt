a80b32a7721105b026d0ea662b8066f8d5e9224b
hasanforaty <hasanforaty@gmail.com>
2022-05-26 11:30:06 +0430

    add material 3 to theme

--- app/src/main/java/com/hasan/foraty/calculator/ui/theme/Theme.kt
+++ app/src/main/java/com/hasan/foraty/calculator/ui/theme/Theme.kt
@@ -1,21 +1,19 @@
 package com.hasan.foraty.calculator.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val DarkColorPalette = darkColorScheme(
+    primaryContainer = Purple200,
+    secondaryContainer = Teal200
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val LightColorPalette = lightColorScheme(
+    primaryContainer = Purple500,
+    secondaryContainer = Teal200
 
     /* Other default colors to override
     background = Color.White,
@@ -35,8 +33,9 @@ fun CalculatorTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Compos
         LightColorPalette
     }
 
+
     MaterialTheme(
-        colors = colors,
+        colorScheme = colors,
         typography = Typography,
         shapes = Shapes,
         content = content
